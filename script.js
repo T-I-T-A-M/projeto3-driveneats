abd25e08b8a43b2b42b224b75c9b3dfcb9e0dcd8
Thiago <thiagotamello@gmail.com>
2022-04-03 10:55:53 -0300

    nada novo

---
+++ script.js
@@ -0,0 +1,26 @@
+function escolha(elemento){
+    const escolha = document.querySelector(".opcao")
+
+if (escolha !== null){
+elemento.classList.remove("opcao");
+}
+
+
+}
+
+
+// function selecionarJogadores (qtdJogadores){
+// 
+//   }
+
+
+
+//para testar  
+// function xx (xx) {
+// console.log (x)
+//
+
+// function escolha(elemento){
+//    const escolha = document.querySelector(".opcao").querySelector(".CLASSE DE NOVO")
+
+
