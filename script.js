4f54025a784426aa8a313eec12074d5d73f74bd9
Thiago <thiagotamello@gmail.com>
2022-04-03 13:27:24 -0300

    selecao de itens

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
-function escolha(elemento){
-    const escolha = document.querySelector(".opcao")
+function escolhaComida(elemento){
+const comidaescolhida = document.querySelector(".opcao-comida")
 
-if (escolha !== null){
-elemento.classList.remove("opcao");
-}
-
+if (comidaescolhida !== null){
+comidaescolhida.classList.remove("opcao-comida")
 
 }
+elemento.classList.add("opcao-comida")
+
+}
+
+function escolhaBebida(elemento){
+    const comidaescolhida = document.querySelector(".opcao-bebida")
+    
+    if (comidaescolhida !== null){
+    comidaescolhida.classList.remove("opcao-bebida")
+    
+    }
+    elemento.classList.add("opcao-bebida")
+    
+    }
+
+    function escolhaSobremesa(elemento){
+        const comidaescolhida = document.querySelector(".opcao-sobremesa")
+        
+        if (comidaescolhida !== null){
+        comidaescolhida.classList.remove("opcao-sobremesa")
+        
+        }
+        elemento.classList.add("opcao-sobremesa")
+        
+    }
+
+
+
+
+
+
+
+
 
 
 // function selecionarJogadores (qtdJogadores){
 // 
 //   }
-
 
 
 //para testar  
