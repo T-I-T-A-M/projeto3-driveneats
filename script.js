c51c1f1d569c4c72ff7da3a6b7aebea49d2af773
Thiago <thiagotamello@gmail.com>
2022-04-04 13:22:15 -0300

    Mensagem e bonus1.0

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let x = "2,50"
+
 function escolhaComida(elemento){
 const comidaescolhida = document.querySelector(".opcao-comida")
 
@@ -43,30 +45,45 @@ function escolhaBebida(elemento){
     
     document.querySelector(".fazer-pedido").classList.add("pedido-pronto")
     document.querySelector(".pedido-pronto").innerHTML="Fechar pedido";
-    document.querySelector("a").classList.add("clicar-whats")
+    document.querySelector(".pedido-realizado").classList.add("clicar-whats")
+
+   
+
+
+
+
+    }
     }
 
+function dadosdopedido(){
+    let nomedacomida = document.querySelector(".opcao-comida").querySelector(".texto-prato").querySelector("h4").innerHTML
+    let nomedabebida = document.querySelector(".opcao-bebida").querySelector(".texto-prato").querySelector("h4").innerHTML
+    let nomedasobremesa = document.querySelector(".opcao-sobremesa").querySelector(".texto-prato").querySelector("h4").innerHTML
+    
+    let precocomida = document.querySelector(".opcao-comida").querySelector(".texto-prato").querySelector(".preco").querySelector("h6").innerHTML.replace(",",".")
+    let precobebida = document.querySelector(".opcao-bebida").querySelector(".texto-prato").querySelector(".preco").querySelector("h6").innerHTML.replace(",",".")
+    let precosobremesa = document.querySelector(".opcao-sobremesa").querySelector(".texto-prato").querySelector(".preco").querySelector("h6").innerHTML.replace(",",".")
+
+    precocomida = Number(precocomida)
+    precocomida = Number(precobebida)
+    precosobremesa = Number(precosobremesa)
+
+    let valorfinal = (Number(precocomida)+Number(precobebida)+Number(precosobremesa)).toFixed(2)
+
+   let nome = prompt("Qual é o seu nome?")
+   let rua = prompt("Qual o seu endereço?")
+
+const textofinal= encodeURIComponent(`Olá, gostaria de fazer o pedido:
+- Prato: ${nomedacomida}
+- Bebida: ${nomedabebida}
+- Sobremesa: ${nomedasobremesa}
+Total: R$ ${valorfinal}
+
+Nome: ${nome}
+Endereço: ${rua}`)
+
+const url = `https://wa.me/5543996527979?text=${textofinal}`
+
+window.open(`https://wa.me/5543996527979?text=${textofinal}`)
 
 }
-
-
-
-
-
-
-
-
-// function selecionarJogadores (qtdJogadores){
-// 
-//   }
-
-
-//para testar  
-// function xx (xx) {
-// console.log (x)
-//
-
-// function escolha(elemento){
-//    const escolha = document.querySelector(".opcao").querySelector(".CLASSE DE NOVO")
-
-
